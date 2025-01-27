aea71f6d268a5a0b8b83b09fc25aea90aed6a144
Till Gohl <till.crapp@gmail.com>
2025-01-28 00:09:06 +0100

    add theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from '@/assets/components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ThemeProvider defaultTheme={'system'} storageKey={'vite-ui-theme'}><App /></ThemeProvider>
   </StrictMode>,
 )
